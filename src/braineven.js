b798c7240ab1967ba973f86c6daa955124a160b2
Sergey <snugryumov@gmail.com>
2019-01-16 17:00:04 +0300

    refactor braineven.js

--- src/braineven.js
+++ src/braineven.js
@@ -1,30 +1,31 @@
 import readlineSync from 'readline-sync';
 
+const numberOfRounds = 3;
+const isEven = number => number % 2 === 0;
+
 const gameEven = () => {
-  console.log('\nWelcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
+  const greetingUser = '\nWelcome to the Brain Games!';
+  const descriptionGame = 'Answer "yes" if number even otherwise answer "no".\n';
+  console.log(greetingUser);
+  console.log(descriptionGame);
 
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
 
-  const numberOfRounds = 3;
-
   for (let i = 0; i < numberOfRounds; i += 1) {
-    const generateNumber = Math.floor(Math.random() * 100);
+    const question = Math.floor(Math.random() * 100);
 
-    const answer = readlineSync.question(`Question: ${generateNumber} `);
+    const answer = readlineSync.question(`Question: ${question} `);
     console.log(`Your answer: ${answer}`);
 
-    const isEven = number => number % 2 === 0;
-    const correctAnswer = isEven(generateNumber) ? 'yes' : 'no';
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
 
     if (answer !== correctAnswer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
-
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    }
+    console.log('Correct!');
   }
 
   console.log(`Congratulations, ${userName}!`);
